45ac5de85faf4bc065948b903fc99176b0f48512
hamody19 <mohammadalslamy@hotmail.com>
2023-05-06 22:50:16 +0300

    fix scroll issue

--- script/script.js
+++ script/script.js
@@ -7,10 +7,11 @@ function scrollFunction() {
     document.body.scrollTop > 20 ||
     document.documentElement.scrollTop > 20
   ) {
-    document.getElementById("navigation-bar-behaver").style.background = "rgba(255, 255, 255, 0.5)";
+    document.getElementById("navigation-bar-behaver").style.background = "rgba(255, 255, 255, 0.9)";
     document.getElementById("amal-logo").style.color = "#000000";
+    document.getElementsByClassName("navigation").style.color = "#000000";
   } else {
     document.getElementById("navigation-bar-behaver").style.background = "rgba(0,0,0,0)";
-    document.getElementById("amal-logo").style.color = "#ffffff"; // Change the color back to white
+    document.getElementById("amal-logo").style.color = "#3A6Cf4"; // Change the color back to white
   }
 }

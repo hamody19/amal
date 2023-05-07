e3e34a99a94bd0fefe669b48faf9a0f51e36e77d
hamody19 <mohammadalslamy@hotmail.com>
2023-05-08 01:38:20 +0300

    delete broken line

--- script/script.js
+++ script/script.js
@@ -9,7 +9,6 @@ function scrollFunction() {
   ) {
     document.getElementById("navigation-bar-behaver").style.background = "rgba(255, 255, 255, 0.9)";
     document.getElementById("amal-logo").style.color = "#000000";
-    document.getElementsByClassName("navigation").style.color = "#000000";
   } else {
     document.getElementById("navigation-bar-behaver").style.background = "rgba(0,0,0,0)";
     document.getElementById("amal-logo").style.color = "#3A6Cf4"; // Change the color back to white
